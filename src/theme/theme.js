36b3c7a8764ed5e4c1a2f6ffc9b3bfb56e4f1d2e
Raja Anas <rajaanas786@gmail.com>
2025-12-10 15:29:11 +0400

    fix 3

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,16 +1,16 @@
-import Logo1 from '../assets/images/theme/Yellow@6x.png';
-import Logo2 from '../assets/images/theme/Purple@6x.png';
-import Logo3 from '../assets/images/theme/Orange@6x.png';
-import Logo4 from '../assets/images/theme/Purple 2@6x.png';
-import Logo5 from '../assets/images/theme/banner/HAlloween Logo V1.png';
-import Logo6 from '../assets/images/theme/Asset 1@4x.png';
+// import Logo1 from '../assets/images/theme/Yellow@6x.png';
+// import Logo2 from '../assets/images/theme/Purple@6x.png';
+// import Logo3 from '../assets/images/theme/Orange@6x.png';
+// import Logo4 from '../assets/images/theme/Purple 2@6x.png';
+// import Logo5 from '../assets/images/theme/banner/HAlloween Logo V1.png';
+// import Logo6 from '../assets/images/theme/Asset 1@4x.png';
 import Logo8 from '../assets/images/theme/theme6/Main-08-12-25.webp'
-import banner1 from '../assets/images/theme/banner/Banners Yellow.webp';
-import banner2 from '../assets/images/theme/banner/Banners-Sky-blue.webp';
-import banner3 from '../assets/images/theme/banner/Banners-orange-2.webp';
-import banner4 from '../assets/images/theme/banner/Banners-purple-2.webp';
-import banner5 from '../assets/images/theme/banner/Banners Halloween 2.webp';
-import banner6 from '../assets/images/theme/banner/Bannersmorron.webp';
+// import banner1 from '../assets/images/theme/banner/Banners Yellow.webp';
+// import banner2 from '../assets/images/theme/banner/Banners-Sky-blue.webp';
+// import banner3 from '../assets/images/theme/banner/Banners-orange-2.webp';
+// import banner4 from '../assets/images/theme/banner/Banners-purple-2.webp';
+// import banner5 from '../assets/images/theme/banner/Banners Halloween 2.webp';
+// import banner6 from '../assets/images/theme/banner/Bannersmorron.webp';
 import Banner9 from '../assets/images/theme/theme6/Main-08-12-25.webp'; 
 
 export const themes = {
@@ -24,54 +24,54 @@ export const themes = {
       textColor: '#000000'
     }
   },
-  3: {
-    logo: Logo6,
-    navbarBg: '#6F1123',
-    bannerKey: banner6,
-    bannerBg: '#6F1123',
-    lightningBanner: {
-      backgroundColor: '#FF3300',
-      textColor: '#000000'
-    }
-  },
-  4: {
-    logo: Logo2,
-    navbarBg: '#37AADA',
-    bannerKey: banner2,
-    bannerBg: '#37AADA',
-    lightningBanner: {
-      backgroundColor: '#1C921C',
-      textColor: '#FFFFFF'
-    }
-  },
-  5: {
-    logo: Logo3,
-    navbarBg: '#E46000',
-    bannerKey: banner3,
-    bannerBg: '#FD6D05',
-    lightningBanner: {
-      backgroundColor: '#FF8A36',
-      textColor: '#FFFFFF'
-    }
-  },
-  6: {
-    logo: Logo4,
-    navbarBg: '#B743C2',
-    bannerKey: banner4,
-    bannerBg: '#9F1DAB',
-    lightningBanner: {
-      backgroundColor: '#6C3BFF',
-      textColor: '#FFFFFF'
-    },
-      7: {
-    logo: Logo5,
-    navbarBg: '#1A1A1A',
-    bannerKey: banner5,
-    bannerBg: '#10031C',
-    lightningBanner: {
-      backgroundColor: '#FF3300',
-      textColor: '#000000'
-    }
-  },
-  }
+  // 3: {
+  //   logo: Logo6,
+  //   navbarBg: '#6F1123',
+  //   bannerKey: banner6,
+  //   bannerBg: '#6F1123',
+  //   lightningBanner: {
+  //     backgroundColor: '#FF3300',
+  //     textColor: '#000000'
+  //   }
+  // },
+  // 4: {
+  //   logo: Logo2,
+  //   navbarBg: '#37AADA',
+  //   bannerKey: banner2,
+  //   bannerBg: '#37AADA',
+  //   lightningBanner: {
+  //     backgroundColor: '#1C921C',
+  //     textColor: '#FFFFFF'
+  //   }
+  // },
+  // 5: {
+  //   logo: Logo3,
+  //   navbarBg: '#E46000',
+  //   bannerKey: banner3,
+  //   bannerBg: '#FD6D05',
+  //   lightningBanner: {
+  //     backgroundColor: '#FF8A36',
+  //     textColor: '#FFFFFF'
+  //   }
+  // },
+  // 6: {
+  //   logo: Logo4,
+  //   navbarBg: '#B743C2',
+  //   bannerKey: banner4,
+  //   bannerBg: '#9F1DAB',
+  //   lightningBanner: {
+  //     backgroundColor: '#6C3BFF',
+  //     textColor: '#FFFFFF'
+  //   },
+  //     7: {
+  //   logo: Logo5,
+  //   navbarBg: '#1A1A1A',
+  //   bannerKey: banner5,
+  //   bannerBg: '#10031C',
+  //   lightningBanner: {
+  //     backgroundColor: '#FF3300',
+  //     textColor: '#000000'
+  //   }
+  // },
+  // }
 };
